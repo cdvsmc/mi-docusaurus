8e261f4f7c7595f56dd5a5ee3067fc91f2c2a18d
cdvsmc <215961361+cdvsmc@users.noreply.github.com>
2025-10-27 12:41:51 -0600

    Update baseUrl in docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://cdvsmc.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/midocusaurus/',
+  baseUrl: '/mi-docusaurus/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
